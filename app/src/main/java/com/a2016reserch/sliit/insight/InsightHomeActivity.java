be08e7425f209eed2868b3ee20bcbbb1cbadc369
Ganindu Ranasinghe <ganindu.prabuddha@outlook.com>
2016-10-29 15:41:51 +0530

    Initial commit

---
+++ app/src/main/java/com/a2016reserch/sliit/insight/InsightHomeActivity.java
@@ -0,0 +1,13 @@
+package com.a2016reserch.sliit.insight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsightHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insight_home);
+    }
+}
