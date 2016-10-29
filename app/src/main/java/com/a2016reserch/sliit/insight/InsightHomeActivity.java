55f87b77398653c0c3f01b1c6b2f2d922c0b416f
Ganindu Ranasinghe <ganindu.prabuddha@outlook.com>
2016-10-29 16:02:52 +0530

    add a comment

--- app/src/main/java/com/a2016reserch/sliit/insight/InsightHomeActivity.java
+++ app/src/main/java/com/a2016reserch/sliit/insight/InsightHomeActivity.java
@@ -3,6 +3,7 @@ package com.a2016reserch.sliit.insight;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 // Add a comment
+// add branche home confi
 public class InsightHomeActivity extends AppCompatActivity {
 
     @Override
