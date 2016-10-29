082f574d224b1d80704c29eb153c1da4e1baf37b
gpProgrammer <ganindu.prabuddha@outlook.com>
2016-10-29 16:05:28 +0530

    Update InsightHomeActivity.java

--- app/src/main/java/com/a2016reserch/sliit/insight/InsightHomeActivity.java
+++ app/src/main/java/com/a2016reserch/sliit/insight/InsightHomeActivity.java
@@ -3,6 +3,7 @@ package com.a2016reserch.sliit.insight;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 // Add a comment
+// added branch
 public class InsightHomeActivity extends AppCompatActivity {
 
     @Override
