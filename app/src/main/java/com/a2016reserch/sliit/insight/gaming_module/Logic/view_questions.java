d18ee8af004f78de36bbdbabdca87bb0ac2d3d06
Sandasala <sandu91nish@gmail.com>
2016-11-06 08:16:57 +0530

    Add Easy level of the game and modify some codes for relevant activities

---
+++ app/src/main/java/com/a2016reserch/sliit/insight/gaming_module/Logic/view_questions.java
@@ -0,0 +1,552 @@
+package com.a2016reserch.sliit.insight.gaming_module.Logic;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.a2016reserch.sliit.insight.R;
+import com.a2016reserch.sliit.insight.gaming_module.view_marks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class view_questions extends Activity implements  TextToSpeech.OnInitListener, View.OnClickListener {
+
+    private DBHelper dbClass;
+    String type=null;
+    private TextToSpeech tts;
+    private GestureDetector mGestureDetector;
+
+    String passedArg=null;
+    int qid;
+    int seq=0;
+    int marks=0;
+    String status=null;
+    boolean isStarted=false;
+    private List<Integer> mClickedButtonIds = new ArrayList<Integer>();
+    private List<Marks> m = new ArrayList<Marks>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_questions);
+
+        dbClass = new DBHelper(this);
+
+
+        Questions quz_One_Easy = new Questions(1, "What is the fist letter of word Ball?","easy","10,30",1);
+        Questions quz_Two_Easy =new Questions(2, "Mark the dots corresponding to letter D?","easy","10,20,40",1);
+        Questions quz_Three_Easy =new Questions(3, "Mark the dots corresponding to number 3?","easy","10,20",1);
+        Questions quz_Four_Easy =new Questions(4, "Mark the dots corresponding to comma?","easy","10",1);
+        Questions quz_Five_Easy =new Questions(5, "How many points do you use for showing braille?","easy","10,20",1);
+        Questions quz_Six_Easy =new Questions(6, "What is the first letter of word ice cream?","easy","10,30,50",1);
+        Questions quz_Seven_Easy =new Questions(7, "What is the result of calculation 1+8?","easy","20,30",1);
+        Questions quz_Eight_Easy =new Questions(8, "What is the missing letter word Mango. M,A,N,G,blank?","easy","10,40,50",1);
+        Questions quz_Nine_Easy =new Questions(9, "Mark the dots corresponding dots for letter U?","easy","10,50,60",1);
+        Questions quz_Ten_Easy =new Questions(10, "What is the result of calculation 2 into 3?","easy","10,20,30",1);
+
+
+
+        Questions quz_One_Medium = new Questions(1, "Which number should come next in this series?  1,3,5,7,9,...'","medium","10,10",2);
+        Questions quz_Two_Medium = new Questions(2, "Fill the missing letters of word doll, D, O,blank, blank?","medium","10,30,50,10,30,50",6);
+
+
+
+        Questions quz_One_Hard = new Questions(1, "If you rearrange the letters C,I,F,A,I,P,C, you would have the name of","hard","10,40,50",1);
+        Questions quz_One_Hard_Sub1 = new Questions(1, "If you rearrange the letters C,I,F,A,I,P,C,you would have the name of","hard","10,20",2);
+        Questions quz_One_Hard_Sub2 = new Questions(1, "If you rearrange the letters C,I,F,A,I,P,C, you would have the name of","hard","10,40",3);
+        Questions quz_One_Hard_Sub3 = new Questions(1, "If you rearrange the letters C,I,F,A,I,P,C, you would have the name of","hard","10",4);
+        Questions quz_One_Hard_Sub4 = new Questions(1, "If you rearrange the letters C,I,F,A,I,P,C, you would have the name of","hard","10,20,40,50",5);
+
+        Questions quz_Two_Hard=new Questions(2,"Type the corresponding letters of word Cat","hard","10,20",1);
+        Questions quz_Two_Hard_Sub1=new Questions(2,"Type the corresponding letters of word Cat","hard","10",2);
+        Questions quz_Two_Hard_Sub2=new Questions(2,"Type the corresponding letters of word Cat","hard","20,30,40,50",3);
+
+
+        dbClass.open();
+
+        dbClass.insertQuestions(quz_One_Easy);
+        dbClass.insertQuestions(quz_Two_Easy);
+        dbClass.insertQuestions(quz_Three_Easy);
+        dbClass.insertQuestions(quz_Four_Easy);
+        dbClass.insertQuestions(quz_Five_Easy);
+        dbClass.insertQuestions(quz_Six_Easy);
+        dbClass.insertQuestions(quz_Seven_Easy);
+        dbClass.insertQuestions(quz_Eight_Easy);
+        dbClass.insertQuestions(quz_Nine_Easy);
+        dbClass.insertQuestions(quz_Ten_Easy);
+
+        dbClass.insertQuestions(quz_One_Medium);
+        dbClass.insertQuestions(quz_Two_Medium);
+
+
+        dbClass.insertQuestions(quz_One_Hard);
+        dbClass.insertQuestions(quz_One_Hard_Sub1);
+        dbClass.insertQuestions(quz_One_Hard_Sub2);
+        dbClass.insertQuestions(quz_One_Hard_Sub3);
+        dbClass.insertQuestions(quz_One_Hard_Sub4);
+
+        dbClass.insertQuestions(quz_Two_Hard);
+        dbClass.insertQuestions(quz_Two_Hard_Sub1);
+        dbClass.insertQuestions(quz_Two_Hard_Sub2);
+
+
+
+        tts=new TextToSpeech(this,this);
+
+        Android_Gesture_Detector  android_gesture_detector  =  new Android_Gesture_Detector();
+        mGestureDetector = new GestureDetector(this, android_gesture_detector);
+
+        passedArg = getIntent().getExtras().getString("type");
+
+        Cursor cursor=dbClass.getQuestionIds(passedArg);
+
+        if (cursor.moveToFirst()){
+
+            qid=cursor.getInt(cursor.getColumnIndex("id"));
+
+        }
+
+
+        Cursor cursor1 = dbClass.retrieveMaxSeqNo(passedArg);
+
+        if (cursor1.moveToFirst()) {
+
+            seq = cursor1.getInt(cursor1.getColumnIndex("sequence"));
+        }
+
+        dbClass.close();
+
+
+        switch (passedArg){
+            case "easy":
+
+                Thread logoTimer = new Thread() {
+                    public void run() {
+                        try {
+
+                            sleep(2000);
+                            speakWords("You have selected Easy level ");
+                            sleep(2000);
+                            speakWords("Let's check your IQ knowledge on easy level");
+
+                            dbClass.open();
+                            Cursor cursor=dbClass.getAllQuestions("easy",qid);
+
+                            if (cursor.moveToFirst()){
+
+                                speakWords("First Question is " + cursor.getString(cursor.getColumnIndex("question")));
+                                //  giveInputs("easy");
+
+
+                            }
+
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                logoTimer.start();
+                break;
+
+            case "medium":
+
+                Thread logoTimer2 = new Thread() {
+                    public void run() {
+                        try {
+
+                            sleep(2000);
+                            speakWords("You have selected Medium level ");
+                            sleep(3000);
+                            speakWords("Let's check your IQ knowledge in medium level");
+
+                            dbClass.open();
+                            Cursor cursor=dbClass.getAllQuestions("medium",qid);
+                            if (cursor.moveToFirst()){
+
+                                speakWords("First Question is " + cursor.getString(cursor.getColumnIndex("question")));
+
+                            }
+
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                logoTimer2.start();
+                break;
+
+            case "hard":
+
+
+
+                Thread logoTimer3 = new Thread() {
+                    public void run() {
+                        try {
+
+                            sleep(2000);
+                            speakWords("You have selected Hard level ");
+                            sleep(3000);
+                            speakWords("Let's check your IQ knowledge in hard level");
+
+                            dbClass.open();
+                            Cursor cursor=dbClass.getAllQuestions("hard",qid);
+
+                            if (cursor.moveToFirst()){
+
+                                speakWords("First Question is " + cursor.getString(cursor.getColumnIndex("question")));
+                            }
+
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                logoTimer3.start();
+
+                break;
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.ENGLISH);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            }
+            else {
+
+            }
+
+        } else {
+            Log.e("TTS", "Initialization Failed!");
+        }
+    }
+
+    private void speakWords(String speech) {
+
+        if (tts != null) {
+
+            tts.setSpeechRate(0.9f);
+            tts.setPitch(1.0f);
+
+            tts.speak(speech, TextToSpeech.QUEUE_ADD, null);
+        }
+        while (tts.isSpeaking() ) {
+        };
+    }
+
+    public boolean QuzRetrieve()
+    {
+        dbClass.open();
+
+        qid++;
+        Cursor cursor = dbClass.getAllQuestions(passedArg,qid);
+
+        if (cursor.moveToFirst()){
+
+            speakWords("Next question is " + cursor.getString(cursor.getColumnIndex("question")));
+
+        }
+
+        return true;
+    }
+
+    public void giveInputs(String type)
+    {
+
+        dbClass.open();
+
+
+
+        int[] input = new int[seq];
+
+        if(type=="easy")
+        {
+            if(seq==1) {
+                speakWords("Please type the correct answer");
+            }
+
+        }
+
+        else if(type=="medium")
+        {
+            if(seq>1) {
+                if(seq==1){
+                    speakWords("Please type the first digit of the number");
+                }
+
+
+            }
+        }
+
+        else if(type=="hard")
+        {
+
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (!isStarted) {
+            isStarted = true;
+            String a=(String) view.getTag();
+            int id= Integer.parseInt(a);
+            mClickedButtonIds.add(id);
+            new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            isStarted = false;
+                            CheckSpellingEasy(mClickedButtonIds);
+                            mClickedButtonIds.clear();
+                        }
+                    },
+                    3000);
+
+        }
+
+        else {
+            String a=(String) view.getTag();
+            int id= Integer.parseInt(a);
+            mClickedButtonIds.add(id);
+        }
+    }
+
+    public boolean CheckSpellingEasy(List<Integer> number) {
+
+        int[] intArray = new int[number.size()];
+
+        for (int i = 0; i < number.size(); i++) {
+            intArray[i] = number.get(i);
+
+        }
+
+
+        dbClass.open();
+
+        String[] result = dbClass.getAnswers(qid,passedArg);
+        String[] array = result[0].split(",");
+
+        for (String elements : array) {
+            System.out.print(elements);
+            int[] arr = new int[array.length];
+
+            int size = arr.length;
+            System.out.print(size);
+
+            for (int j = 0; j < arr.length; j++) {
+
+                arr[j] = Integer.parseInt(array[j]);
+            }
+
+            if (Arrays.equals(intArray, arr)) {
+
+                status = "correct";
+                marks = 10;
+                Marks marks1 = new Marks(qid, passedArg, status, marks);
+
+                m.add(marks1);
+                retrieveValuesFromListMethod2(m);
+
+                if(Arrays.equals(intArray, arr)) {
+                    break;
+                }
+            }
+
+            else
+            {
+
+                status="incorrect";
+                int finalCount=0;
+
+                if(m.size()>0)
+                {
+                    for(int i=0;i<m.size();i++)
+                    {
+                        Marks mark = m.get(i);
+                        finalCount = finalCount + mark.getMarks();
+                    }
+                }
+                else
+                {
+                    finalCount=0;
+                }
+
+                Thread logoTimer3 = new Thread() {
+                    public void run() {
+                        try {
+
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                logoTimer3.start();
+
+                Intent n1 = new Intent(view_questions.this,view_marks.class);
+                n1.putExtra("marks",String.valueOf(finalCount));
+                startActivity(n1);
+
+            }
+        }
+
+
+        if(status=="correct") {
+            QuzRetrieve();
+        }
+
+        return true;
+    }
+
+    public boolean AddMarks(int id,String difficulty)
+    {
+
+        return true;
+    }
+
+    public static void retrieveValuesFromListMethod2(List list)
+    {
+        //Retrieving values from list
+        int size = list.size();
+        for(int i=0;i<size;i++)
+        {
+            System.out.println(list.get(i));
+        }
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        mGestureDetector.onTouchEvent(event);
+
+        return super.onTouchEvent(event);
+
+
+    }
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    class Android_Gesture_Detector implements GestureDetector.OnGestureListener,
+            GestureDetector.OnDoubleTapListener {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Log.d("Gesture ", " onDown");
+
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Log.d("Gesture ", " onSingleTapConfirmed");
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            Log.d("Gesture ", " onSingleTapUp");
+            return true;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+            Log.d("Gesture ", " onShowPress");
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Log.d("Gesture ", " onDoubleTap");
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            Log.d("Gesture ", " onDoubleTapEvent");
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Log.d("Gesture ", " onLongPress");
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+
+            Log.d("Gesture ", " onScroll");
+            if (e1.getY() < e2.getY()){
+                Log.d("Gesture ", " Scroll Down");
+            }
+            if(e1.getY() > e2.getY()){
+                Log.d("Gesture ", " Scroll Up");
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (e1.getX() < e2.getX()) {
+                Log.d("Gesture ", "Left to Right swipe: "+ e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", String.valueOf(velocityX) + " pixels/second");
+//
+//                Intent intent = new Intent(view_questions.this, sanda.class);
+//                intent.putExtra("difficulty", passedArg);
+//                startActivity(intent);
+
+
+            }
+            if (e1.getX() > e2.getX()) {
+                Log.d("Gesture ", "Right to Left swipe: "+ e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", String.valueOf(velocityX) + " pixels/second");
+
+            }
+            if (e1.getY() < e2.getY()) {
+                Log.d("Gesture ", "Up to Down swipe: " + e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", String.valueOf(velocityY) + " pixels/second");
+
+            }
+            if (e1.getY() > e2.getY()) {
+                Log.d("Gesture ", "Down to Up swipe: " + e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", String.valueOf(velocityY) + " pixels/second");
+            }
+            return true;
+
+        }
+    }
+}
--- app/src/main/java/com/a2016reserch/sliit/insight/gaming_module/MainMenu_GameLevels.java
+++ app/src/main/java/com/a2016reserch/sliit/insight/gaming_module/MainMenu_GameLevels.java
@@ -12,14 +12,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.a2016reserch.sliit.insight.R;
-import com.a2016reserch.sliit.insight.gaming_module.Logic.Questions;
-import com.a2016reserch.sliit.insight.gaming_module.Logic.QuestionUpdateEndpoint;
 import com.a2016reserch.sliit.insight.gaming_module.Logic.DBHelper;
+import com.a2016reserch.sliit.insight.gaming_module.Logic.QuestionUpdateEndpoint;
+import com.a2016reserch.sliit.insight.gaming_module.Logic.Questions;
 import com.a2016reserch.sliit.insight.gaming_module.Logic.Update_Question;
 import com.a2016reserch.sliit.insight.gaming_module.Logic.view_questions;
-import com.a2016reserch.sliit.insight.learning_module.help_keypad.Braille_Keypad;
 import com.a2016reserch.sliit.insight.learning_module.help_keypad.Braille_Tutorials;
-import com.a2016reserch.sliit.insight.learning_module.help_keypad.Intro_Keypad_Top;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -30,7 +28,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainMenu_GamingModule extends Activity implements TextToSpeech.OnInitListener {
+public class MainMenu_GameLevels extends Activity implements  TextToSpeech.OnInitListener{
 
     private int start = -1;  // main items counter variable
     private TextView name; // text view variable
@@ -40,11 +38,12 @@ public class MainMenu_GamingModule extends Activity implements TextToSpeech.OnIn
     // This code can be any value you want, its just a checksum.
     private static final int MY_DATA_CHECK_CODE = 1234;
     public static String lastCommand = null;
+    String type=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu__gaming_module);
+        setContentView(R.layout.activity_main_menu__game_levels);
 
         // Android Gestures
         Android_Gesture_Detector android_gesture_detector = new Android_Gesture_Detector();
@@ -57,14 +56,14 @@ public class MainMenu_GamingModule extends Activity implements TextToSpeech.OnIn
 
         // change text view value
         name = (TextView) findViewById(R.id.txtName);
-        name.setText("Welcome to Gaming Module");
+        name.setText("Gaming Levels");
 
         Thread logoTimer = new Thread() {
             public void run() {
                 try {
 
                     sleep(1500);
-                    speakWords("Welcome to Gaming Module");
+                    speakWords("Gaming Levels");
 
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -228,8 +227,10 @@ public class MainMenu_GamingModule extends Activity implements TextToSpeech.OnIn
             Log.d("Gesture ", " onDoubleTap");
             int val = start;
             if (start == 0) {
-                speakWords("Start Game");
-                Intent intent = new Intent(MainMenu_GamingModule.this, MainMenu_GameLevels.class);
+                speakWords("Easy Level is opening");
+                type="easy";
+                Intent intent = new Intent(MainMenu_GameLevels.this, view_questions.class);
+                intent.putExtra("type", "easy");
                 startActivity(intent);
                 onDestroy();
 
@@ -239,53 +240,18 @@ public class MainMenu_GamingModule extends Activity implements TextToSpeech.OnIn
 
             } else if (start == 1) {
 
-                speakWords("Update Game");
-                speakWords("Wait a moment, game will be started in few minutes");
-//                Intent intent = new Intent(MainMenu_GamingModule.this, Braille_Keypad.class);
-//                startActivity(intent);
-//                onDestroy();
-
-                Retrofit.Builder retrofitBuilder = new Retrofit.Builder();
-                Retrofit retrofit = retrofitBuilder.baseUrl("http://192.168.2.10:8084/insight/webapi/").addConverterFactory(GsonConverterFactory.create()).build();
-
-                final QuestionUpdateEndpoint updateQues = retrofit.create(QuestionUpdateEndpoint.class);
-
-                Call<ArrayList<Update_Question>> getUserLoginCall = updateQues.getQues();
-                getUserLoginCall.enqueue(new Callback<ArrayList<Update_Question>>() {
-
-                    @Override
-                    public void onResponse(Call<ArrayList<Update_Question>> call, Response<ArrayList<Update_Question>> response) {
-
-                        DBHelper helper = new DBHelper(MainMenu_GamingModule.this);
-                        helper.open();
-
-                        helper.clearTable();
-
-                        for (int i = 0; i < response.body().size(); i++) {
-
-                            Questions newQuz = new Questions();
-                            newQuz.setQuizId(response.body().get(i).getQuzId());
-                            newQuz.setAnswers(response.body().get(i).getAnswer());
-                            newQuz.setQuestion(response.body().get(i).getQuestion());
-                            newQuz.setDifficulty(response.body().get(i).getLevel());
-                            newQuz.setSequence(response.body().get(i).getSequence());
-                            helper.insertQuestions(newQuz);
-                        }
-                        helper.close();
-                        speakWords("Questions have Updated successfully");
-                    }
-
-                    @Override
-                    public void onFailure(Call<ArrayList<Update_Question>> call, Throwable t) {
-                        Toast.makeText(MainMenu_GamingModule.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-                        Toast.makeText(MainMenu_GamingModule.this, "server error", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                speakWords("Medium Level is opening");
+                type="medium";
+                Intent intent = new Intent(MainMenu_GameLevels.this, view_questions.class);
+                intent.putExtra("type", "medium");
+                startActivity(intent);
 
 
             } else if (start == 2) {
-                speakWords("Instructions");
-                Intent intent = new Intent(MainMenu_GamingModule.this, Braille_Tutorials.class);
+                speakWords("Hard Level is opening");
+                type="hard";
+                Intent intent = new Intent(MainMenu_GameLevels.this, Braille_Tutorials.class);
+                intent.putExtra("type", "hard");
                 startActivity(intent);
                 onDestroy();
 
@@ -403,16 +369,16 @@ public class MainMenu_GamingModule extends Activity implements TextToSpeech.OnIn
 
     public void viewText(int no) {
         if (no == 0) {
-            name.setText("Start Game");
-            speakWords("Start Game");
+            name.setText("Easy");
+            speakWords("Easy Level");
 
         } else if (no == 1) {
-            name.setText("Update Game");
-            speakWords("Update Game");
+            name.setText("Medium");
+            speakWords("Medium Level");
 
         } else if (no == 2) {
-            name.setText("Instructions");
-            speakWords("Instructions");
+            name.setText("Hard");
+            speakWords("Hard Level");
         }
 
 
